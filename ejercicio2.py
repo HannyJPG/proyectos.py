cd9d8d0267c9838f003399967c784d585c3e8497
Hanny <melanyhanady@gmail.com>
2025-07-27 00:24:37 -0500

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,40 @@
+#DEfinimos una funcion llamada operaxiones
+def operaciones():
+    print("Bienvenido Menu de operaciones ")
+    print("1 : Sumar")
+    print("2: Restar")
+    print("3 : Multiplicar")
+    print("4 : Dividir ")
+    print("5 : Salir ")
+    
+#Usaremos la funcion do while para las operaciones 
+while True :  
+    operaciones()
+    opcion=input("Elija una opcion del 1 al 5: ")
+    if opcion =="5":
+        print(" Adios , muchas gracias uwu")
+        
+#Palabra reservada try q se usa para ejecutar un bloque q puede causar error
+    try:
+        n1 = float("Ingrese primer numero")
+        n2= float("Ingrese asegundo numero")
+        if opcion =="1":
+            resultado = n1+n2
+            print(f"El resultado de la suma es {resultado}")
+        elif opcion =="2":
+            resultado = n1-n2
+            print(f"El resultado de la resta es {resultado}")
+        elif opcion =="3":
+            resultado = n1*n2
+            print(f"El resultado de la resta es {resultado}")
+        elif opcion =="4":
+            if n2!=0:
+                resultado = n1/n2
+                print(f"El resultado de la resta es {resultado}")
+            else :
+                print("no es divisible por cero")
+        else :
+            print("Ingrese un numero valido")   
+    except ValueError:
+        print("no es popsible el dato estra incorrecto")
+    print("-------------------Gracias-------------------------")
